64471dd7e1f4583043db3a715fddc298341f57de
Frank Burnham <seditiosus@debian>
2014-09-17 08:21:48 -0400

    update

--- hackerrank-filter-positions-in-a-list/src/hackerrank_filter_positions_in_a_list/core.clj
+++ hackerrank-filter-positions-in-a-list/src/hackerrank_filter_positions_in_a_list/core.clj
@@ -1,6 +1,17 @@
 (ns hackerrank-filter-positions-in-a-list.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn foo [lst]
+  (loop [l lst
+         ret []
+         i 0]
+    (cond 
+     (empty? l) ret
+     (odd? i) (recur (rest l) (conj ret (first l)) (+ i 1))
+     :else (recur (rest l) ret (+ i 1)))))
+
+;;an alternate solution to study
+(defn bar [lst] 
+  (map second 
+       (filter 
+        (fn [v] (if (odd? (first v)) true false)) 
+        (map-indexed (fn [x y] [x y]) lst))))
