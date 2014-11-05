a289f043c600b976a682569fa3a99c902c28ff1f
Frank Burnham <seditiosus@debian>
2014-11-05 14:16:44 -0500

    improved evaluating e^x for maintainability

--- hackerrank-evaluating-e-x/src/hackerrank_evaluating_e_x/core.clj
+++ hackerrank-evaluating-e-x/src/hackerrank_evaluating_e_x/core.clj
@@ -1,61 +1,60 @@
 (ns hackerrank-evaluating-e-x.core)
 
-;;e^x is given by
-;;1 + x + x^2/2! + x^3/3! + x^n/n!
-;;in this problem we will only use the first 10 terms
+;;the below functions are not very clojure. they are pure
+;;which is nice but they don't think in terms of transforming
+;;lists so let's think about the list transformations and redo
+;;some of this mess
 
 ;;this factorial is a bit quicker with smaller numbers
 ;;compared to the solution using map
 (defn ! 
-  ([n] (! n 1))
+  ([n] (! n 1N))
   ([n ret]
      (if (= n 0) ret
          (recur (dec n) (* ret n)))))
+;;(time (! 100000N))
+;;time taken: "Elapsed time: 23860.643874 msecs"
+
+;;bam
+(defn ! [n]
+  (reduce * (range 1N (inc n))))
+;;(time (! 100000N))
+;;time taken: "Elapsed time: 21521.075593 msecs"
+;;it's almost too close to care about the speed
+;;it is more clojure
 
 (defn pow 
   ([n x] (pow n x 1))
   ([n x ret]
      (if (= x 0) ret
          (recur n (dec x) (* n ret)))))
+;;(time (pow 35N 198370N))
+;;"Elapsed time: 20416.585042 msecs"
+;;fancy that a more sophisticated test needs to be
+;;made up to profile these two functions the more verbose
+;;single list method is obviously quicker
 
-(defn e-expansion 
-  ([x] (e-expansion x 2 (+ 1.0 x)))
-  ([x n ret]
-     (if (= 10 n) (println ret)
-         (recur x (inc n) (+ ret
-                             (/ (pow x n) (! n)))))))
+(defn pow [base exp]
+  (reduce * (repeat exp base)))
+;;(time (pow 35N 198370N))
+;;"Elapsed time: 31361.801696 msecs"
 
-;;now we need a loop to read the number of inputs and run the expansions
-(defn get-inputs
-  ([] (get-inputs (Integer/parseInt (read-line))))
-  ([num-lines]
-     ;;call e-expansion once for each line of input
-     (e-expansion (Float/parseFloat (read-line)))
-     (if (= 0 num-lines) nil
-         (recur (dec num-lines)))))
+;holy shit that needs an update let's try this for readability
+(defmacro read-float []
+  `(Float/parseFloat (read-line)))
 
-;) after that...make it anonymous and submit ;)
-((fn get-inputs [num-lines]
-   (if (= 0 num-lines) nil
-       (do
-         ((fn e-expansion 
-            ([x] (e-expansion x 2 (+ 1.0 x)))
-            ([x n ret]
-               (if (= 10 n) (println ret)
-                   (recur x (inc n) (+ ret
-                                       (/ 
-                                        ((fn pow 
-                                           ([n x] (pow n x 1))
-                                           ([n x ret]
-                                              (if (= x 0) ret
-                                                  (recur n (dec x) (* n ret)))))
-                                         x n) 
-                                        ((fn ! 
-                                           ([n] (! n 1))
-                                           ([n ret]
-                                              (if (= n 0) ret
-                                                  (recur (dec n) (* ret n))))) 
-                                         n)))))))
-          (Float/parseFloat (read-line)))
-         (recur (dec num-lines)))))
-       (Integer/parseInt (read-line)))
+(let [N (read-float)
+      facs (map #(reduce * (range 1N (inc %))) (range 2 10))]
+  (loop [count N]
+    (if (< count 1) nil
+        (let [x (read-float)
+              pows (map #(reduce * (repeat %2 %1)) (repeat x) (range 2 10))]
+          (println (float (+ 1 x (reduce + (map / pows facs)))))
+          (recur (dec count))))))
+ 
+;so with some cleanup the resulting code is more concise and I think
+;easier to read. all you have to think about is how a few lists
+;are going to behave instead of having to step line by line
+;through the old example
+
+;) Isn't lisp sexy ;)
