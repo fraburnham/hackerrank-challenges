d2e3b270ac8c0f03e2d704113f050b982dc76e33
Frank Burnham <seditiosus@debian>
2014-11-05 12:25:06 -0500

    update

--- hackerrank-service-lane/src/hackerrank_service_lane/core.clj
+++ hackerrank-service-lane/src/hackerrank_service_lane/core.clj
@@ -1,6 +1,14 @@
 (ns hackerrank-service-lane.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro subseq [x y s]
+  `(drop ~x (take (inc ~y) ~s)))
+
+(let [T (Integer/parseInt (last (clojure.string/split (read-line) #" ")))
+      lane (map #(Integer/parseInt %)
+                (clojure.string/split (read-line) #" "))]
+  (loop [n T]
+    (if (= 0 n) nil
+        (let [[i j] (map #(Integer/parseInt %)
+                         (clojure.string/split (read-line) #" "))]
+          (println (apply min (subseq i j lane)))
+          (recur (dec n))))))
