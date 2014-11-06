7a3e273c6c23fac1120014899246f97c7ef13080
Frank Burnham <seditiosus@debian>
2014-11-05 21:10:41 -0500

    string-mingling clojure style improvements

--- hackerrank-string-mingling/src/hackerrank_string_mingling/core.clj
+++ hackerrank-string-mingling/src/hackerrank_string_mingling/core.clj
@@ -1,31 +1,5 @@
 (ns hackerrank-string-mingling.core)
 
-(def random (java.util.Random.))
-;define characters list to use to generate string
-(def chars 
-   (map char (concat (range 48 58) (range 66 92) (range 97 123))))
-;generates 1 random character
-(defn random-char [] 
-  (nth chars (.nextInt random (count chars))))
-; generates random string of length characters
-(defn random-string [length]
-  (apply str (take length (repeatedly random-char))))
-
-(def stra (random-string 100000))
-(def strb (random-string 100000))
-(println (reduce str (map str stra strb)))
-
-;just gotta make it read two lines from std in and pass them
-;it's too slow. try to make your own recursive?
-
-(defn string-mingling [stra strb]
-  (if (empty? stra) nil
-      (do
-        (print (first stra))
-        (print (first strb))
-        (recur (rest stra) (rest strb)))))
-
-;as it turns out the map reduce was slower than just
-;outputting it. Could have guessed that probably.
-;two steps are slower than one.
-;map timed out (over 8s) recur's longest time was 3s
+;;woah broah (interleave) is a builtin
+(println (apply str (interleave (seq (read-line)) (seq (read-line)))))
+;;2.78s longest HR.com test case
